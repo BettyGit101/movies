19461992dcb616eeaf03f81463ab6aa52b21091e
unknown <aaa@gmail.com>
2023-05-25 13:11:14 +0300

    Updates

---
+++ precache-manifest.06c52277ef71ca22792281595c49d6a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f59e526d912dd5c13a9",
+    "url": "/movies-FE/static/css/main.42f12a79.chunk.css"
+  },
+  {
+    "revision": "6f59e526d912dd5c13a9",
+    "url": "/movies-FE/static/js/main.345aead8.chunk.js"
+  },
+  {
+    "revision": "2d51fe1782418bf583e8",
+    "url": "/movies-FE/static/js/runtime~main.fb0c533d.js"
+  },
+  {
+    "revision": "ea6f6da818434ea84dbd",
+    "url": "/movies-FE/static/js/2.eea4ac6b.chunk.js"
+  },
+  {
+    "revision": "12cd54d35df871c721f20ff109149706",
+    "url": "/movies-FE/static/media/popcorenIcon.12cd54d3.jpg"
+  },
+  {
+    "revision": "e2cdbd7460b27624efeed92e951a3441",
+    "url": "/movies-FE/index.html"
+  }
+];
